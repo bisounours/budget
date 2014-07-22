5e52f9f88cd1e4894fca2ed3823e5135ba497772
Loïc Tolvai <l.tolvai@symaris.com>
2014-07-22 13:25:50 +0200

    Correction affichage + Nettoyage fichier temporaire + fichier base sqlite

--- tmp/foot.e23cb84e1b9c3a6502dc51d635e22b98.rtpl.php
+++
@@ -1,3 +0,0 @@
-<?php if(!class_exists('raintpl')){exit;}?>	<script type="text/javascript" src="template/composant/../../js/foot.js"></script>
-	</body>
-</html>
--- tmp/liste_categorie.28faa2a03c65dda689fd5608a1fee44c.rtpl.php
+++
@@ -1,9 +0,0 @@
-<?php if(!class_exists('raintpl')){exit;}?><label class="select">
-	<select id="lst_categorie" name="lst_categorie" size="1">
-		<?php $counter1=-1; if( isset($categorie_list) && is_array($categorie_list) && sizeof($categorie_list) ) foreach( $categorie_list as $key1 => $value1 ){ $counter1++; ?>
-
-			<option value="<?php echo $value1["id"];?>" <?php if( $categorie_select && $categorie_select == $value1["id"] ){ ?> selected <?php } ?>><?php echo $value1["libelle"];?></option>
-		<?php } ?>
-
-	</select>
-</label>
